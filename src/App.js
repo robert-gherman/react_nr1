bcbb905f975f1230a85d06b95b4504115fa0e93c
robert gherman <gherman_petru14@yahoo.com>
2022-11-30 20:04:12 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,42 +1,85 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 // Components
 import Song from "./components/Song";
 import Player from "./components/Player";
 // Styles
 import "./styles/app.scss";
-
+import { Credentials } from "./components/Credentials";
+import axios from "axios";
+import { Buffer } from "buffer";
 function App() {
-  const [data, setData] = useState();
-  const spotifyAuth = async () => {
-    const settings = {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization:
-          "Bearer " +
-          "BQAR-sStmnGaPVrSTqMNiZO3vMx1qFzKMcytKpa8ULoyvUgUeY87XZpq5iLj2FYQmGsE9QwzoLqPcVxgoyEVkRxEokbEsz26FgjRv7Ky8kdoI8lojGalCijTS5zothK3G2NwAhDTHygODnThJM8AqygU9jnqQlaPk7CPZ7ZTnCZ6f-FLd4-QTE-BjUAkGp-tLv8",
-      },
-    };
-    const response = await fetch(
-      "https://api.spotify.com/v1/playlists/3cEYpjA9oz9GiPac4AsH4n?market=ES",
-      settings
-    );
-    const data = await response.json();
+  const spotify = Credentials();
 
-    setData(data);
-  };
+  const [accessToken, setAccessToken] = useState("");
+  const [data, setData] = useState();
+  const [tracks, setTracks] = useState();
+  useEffect(() => {
+    //api access token
+    axios
+      .post(
+        "https://accounts.spotify.com/api/token",
+        new URLSearchParams({
+          grant_type: "client_credentials",
+          code: "AQBxWnU0SB...l_jU",
+          redirect_uri: "https://example.com/callback",
+        }).toString(),
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            Authorization:
+              "Basic " +
+              new Buffer(
+                spotify.ClientId + ":" + spotify.ClientSecret
+              ).toString("base64"),
+          },
+        }
+      )
+      .then(function (response) {
+        setAccessToken(response.data.access_token);
+        //console.log(response.data.access_token);
+        //  console.log(accessToken);
+        axios(
+          "https://api.spotify.com/v1/playlists/3cEYpjA9oz9GiPac4AsH4n?market=ES",
+          {
+            method: "GET",
+
+            headers: {
+              Authorization: "Bearer " + accessToken,
+            },
+          }
+        ).then((tracksResponse) => {
+          setTracks(tracksResponse.data);
+          console.log(tracksResponse.data);
+        });
+      });
+
+    //get playlist
+    // const spotifyAuth = async () => {
+    //   const settings = {
+    //     method: "GET",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //       Authorization: "Bearer " + accessToken,
+    //     },
+    //   };
+    //   const response = await fetch(
+    //     "https://api.spotify.com/v1/playlists/3cEYpjA9oz9GiPac4AsH4n?market=ES",
+    //     settings
+    //   );
+    //   const result = await response.json().then();
+    //   setData(data);
+    //   console.log(data);
+    // };
+
+    // // call function
+    // spotifyAuth();
+  }, []);
+
   return (
     <div className="App">
       <Song />
       <Player />
-      <button
-        onClick={() => {
-          spotifyAuth();
-          console.log({ data });
-        }}
-      >
-        Click me
-      </button>
+      <button>Click me</button>
     </div>
   );
 }
